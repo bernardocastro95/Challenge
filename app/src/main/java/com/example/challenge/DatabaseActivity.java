b5c823365910cedccf6d32a5a7e15b64475e3e0a
Bernardo <bernardoc544@gmail.com>
2020-03-20 16:49:23 -0400

    Implemented logout functionality and a menu

--- app/src/main/java/com/example/challenge/DatabaseActivity.java
+++ app/src/main/java/com/example/challenge/DatabaseActivity.java
@@ -2,13 +2,47 @@ package com.example.challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DatabaseActivity extends AppCompatActivity {
+
+    private FirebaseAuth fab;
+    private Button logout;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logoutMenu: {
+                Logout();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        fab = FirebaseAuth.getInstance();
+
+
+    }
+    private void Logout(){
+        fab.signOut();
+        finish();
+        startActivity(new Intent(DatabaseActivity.this, MainActivity.class));
     }
 }
